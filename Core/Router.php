8a443a08555283223b36dcb1b26aaa5d0fad1a64
MohamedMoustir <itsmoustir@gmail.com>
2025-02-11 16:56:06 +0100

    Ajout du répertoire public

--- Core/Router.php
+++ Core/Router.php
@@ -1,47 +1,48 @@
 <?php
-// class Router{
-//     private $controller = 'Home';
-//     private $method     = 'index';
 
-//     private function splitURL()
-//     {
-//         $URL = $_GET['url'] ?? 'home';
-//         $URL = explode("/", trim($URL,"/"));
-//         return $URL;
-//     }
+class Router{
+    private $controller = 'Home';
+    private $method     = 'index';
 
-//     public function loadController()
-//     {
-//         $URL = $this->splitURL();
+    private function splitURL()
+    {
+        $URL = $_GET['url'] ?? 'home';
+        $URL = explode("/", trim($URL,"/"));
+        return $URL;
+    }
 
-//         / select controller /
-//         $filename = "../app/controllers/".ucfirst($URL[0]).".php";
-//         if(file_exists($filename))
-//         {
-//             require $filename;
-//             $this->controller = ucfirst($URL[0]);
-//             unset($URL[0]);
-//         }else{
+    public function loadController()
+    {
+        $URL = $this->splitURL();
 
-//             $filename = "../app/controllers/_404.php";
-//             require $filename;
-//             $this->controller = "_404";
-//         }
+        // / select controller /
+        $filename = "../app/controllers/".ucfirst($URL[0]).".php";
+        if(file_exists($filename))
+        {
+            require $filename;
+            $this->controller = ucfirst($URL[0]);
+            unset($URL[0]);
+        }else{
 
-//         $controller = new $this->controller;
+            $filename = "../app/controllers/_404.php";
+            require $filename;
+            $this->controller = "_404";
+        }
 
-//         / select method /
-//         if(!empty($URL[1]))
-//         {
-//             if(method_exists($controller, $URL[1]))
-//             {
-//                 $this->method = $URL[1];
-//                 unset($URL[1]);
-//             }
-//         }
+        $controller = new $this->controller;
 
-//         call_user_func_array([$controller,$this->method], $URL);
+        // / select method /
+        if(!empty($URL[1]))
+        {
+            if(method_exists($controller, $URL[1]))
+            {
+                $this->method = $URL[1];
+                unset($URL[1]);
+            }
+        }
 
-//     }
+        call_user_func_array([$controller,$this->method], $URL);
 
-// }
+    }
+
+}
